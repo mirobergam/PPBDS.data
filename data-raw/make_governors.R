c47db0d24d63d6c3e4c83ddc5311438e999613cd
Miroslav Bergam <mlbergam@gmail.com>
2020-07-10 10:53:35 -0400

    gender, region, party vars

--- data-raw/make_governors.R
+++ data-raw/make_governors.R
@@ -1,5 +1,7 @@
 library(tidyverse)
 
+# Importing data
+
 z <- read_csv("data-raw/longevity.csv", col_types = cols(
   .default = col_double(),
   area = col_character(),
@@ -12,3 +14,27 @@ z <- read_csv("data-raw/longevity.csv", col_types = cols(
   status = col_character()
 ))
 
+z <- z %>%
+
+  # Gender variable
+
+  mutate(gender = as.character(case_when(
+    female == 0 ~ "male",
+    female == 1 ~ "female"
+  ))) %>%
+
+  # Ideology variable
+
+  mutate(party = as.character(case_when(
+    democrat == 0 ~ "democrat",
+    republican == 1 ~ "republican"
+  ))) %>%
+
+  # Regional variable
+
+  mutate(region = as.character(case_when(
+    reg_south == 1 ~ "south",
+    reg_west == 1 ~ "west",
+    reg_midwest == 1 ~ "midwest",
+    reg_northeast == 1 ~ "northeast"
+  )))
